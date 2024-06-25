e97f272034ee646a1f9c076f838cb5ea2281677c
mile <darimile@gmail.com>
2024-06-25 18:58:49 +0200

    fix guie vuejs

--- vuejs/reference-guide.js
+++ vuejs/reference-guide.js
@@ -1,5 +1,10 @@
 // Vuejs
 
+
+// Para que se vean el nombre de las variables en pantalla
+
+<div>{{ mensaje }}</div> 
+
 /**********************************************************
  * 
  * V  A  R  I  A  B  L  E  S
