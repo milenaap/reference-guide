ea74c7245e68591afb84e16b65694c006584a171
mile <darimile@gmail.com>
2025-02-28 22:18:20 +0100

    reference-guide

--- react/reference-guide.js
+++ react/reference-guide.js
@@ -9,6 +9,8 @@
  * Hook  
  Se importa 
 
+- useState
+
  import { useState } from "react";
 
  Luego en el componente se declara la variable y la funcion que la modifica
@@ -16,11 +18,40 @@
  
 
 
+ Estructura de un JSX:
+
+import React from 'react'                   /// <------- Aquí van los import 
+const formData = {                          /// <------- NO SIEMPRE van la declaraciones de las variables y/o constantes y/o funciones
+    ...
+
+}  
+                     
+ export const reference-guide = () => {
+
+    const dispatch = useDispatch();        /// <------- Aquí van los "use..." Hooks
+
+
+    const formData = {                     /// <------- NO SIEMPRE van la declaraciones de las variables y/o constantes y/o funciones
+    ...
+
+}
+    
+    return (
+     <div>reference-guide</div>            /// <------- Aquí va el HTML y/o renderizaciones y/o llamadas a componentes
+   )
+ }
 
 
 
 
  */
+
+ 
+ 
+
+
+ 
+ 
 
 
 
